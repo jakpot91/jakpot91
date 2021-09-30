54a6227633ba078be50ccc7747692131d19d3192
z20ele18 <z20ele18@z00p3-08.SPSKNM>
2021-09-30 07:57:06 +0200

    posledna hodina

---
+++ Project3/Project3/Source.cpp
@@ -0,0 +1,39 @@
+/*
+#include <iostream>;
+using namespace std;
+int main() {
+	int a;
+	cout << "zadaj a:";
+	cin >> a;
+	int vysledok = a * 2;
+	cout << vysledok << endl;
+
+
+
+	return 0;
+}
+*/
+#include <iostream>;
+using namespace std;
+int main() {
+	int a = 10;
+	int b = 99;
+	int c;
+
+	c = a;
+	a = b;
+	b = c;
+
+
+
+
+	cout << "Hodnota A po vymene" << a << endl;
+	cout << "hodnota B po vymene" << b << endl;
+
+
+	return 0;
+
+}
+
+
+
---
+++ helloworld/helloworld/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main!";
+	cout << "Ahoj svet !";
+	cout << "Ahoj svet !";
+	cout << "Ahoj svet !";
+	cout << "Ahoj svet !";
+		l
+	
+}
